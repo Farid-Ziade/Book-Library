7177a5f3a7e8bb236f27106fff5325ed1a51beee
Farid-Ziade <61830209@students.liu.edu.lb>
2025-12-08 01:44:52 +0200

    Refactor book addition logic and update display of book details

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ let title = document.querySelectorAll("#title");
 let author = document.querySelectorAll("#author");
 let pages = document.querySelectorAll("#pages");
 let read = document.querySelectorAll("#read");
-
 const myLibrary = [];
-
 class Books {
   constructor(title, author, pages, read) {
     this.id = crypto.randomUUID();
@@ -17,29 +15,24 @@ class Books {
 
 function addBook(id, title, author, pages, read) {
   const newBook = new Books(id, title, author, pages, read);
-  myLibrary.unshift(newBook); //add book as first element of the aray aka first one that will show on the webpage
+  myLibrary.push(newBook); //add book as first element of the aray aka first one that will show on the webpage
 }
-addBook("the fault in our stars", "b", 12, false);
-addBook("starwars", "b", 12, true);
-addBook("john", "b", 12, false);
-addBook("cena", "b", 12, true);
-addBook("alo", "b", 12, false);
-addBook("bravo", "b", 12, true);
-console.log(
-  myLibrary.forEach((book) => {
-    title.forEach((title) => {
-      title.textContent = book.title;
-    });
-    author.forEach((author) => {
-      author.textContent = myLibrary[0].author;
-    });
-    pages.forEach((pages) => {
-      pages.textContent = myLibrary[0].pages + " pages";
-    });
-    read.forEach((read) => {
-      read.textContent = myLibrary[0].read ? "read" : "not read";
-    });
-  })
-);
+addBook("Book1", "1", 1, false);
+addBook("Book2", "2", 2, true);
+addBook("Book3", "3", 3, false);
+addBook("Book4", "4", 4, true);
+addBook("Book5", "5", 5, false);
+addBook("Book6", "6", 6, true);
+
+for (let i = 0; i < myLibrary.length; i++) {
+  title[i].textContent = myLibrary[i].title;
+  author[i].textContent = "Written by " + myLibrary[i].author;
+  pages[i].textContent = "# of pages: " + myLibrary[i].pages;
+  read[i].textContent = "status: " + myLibrary[i].read ? "read" : "not read";
+}
 
 //should figure the function how to dynamically add books to the pagee without hardcoding it here, I can already have books in my library that it will display after looping through the array not having it hardcoded in the html. I started with the html hardcoded as "Skeleton" now must think of how to add them dynamically by looping through it also need to add a button that will pop a form asking for the title author pages and if read from the user --refer to the odin project point #4
+
+//above comment was done 12/8/2025 1:44am
+
+//added the logic where I have some books by default in my library need now to do #4 of hte odin project wheere I need to add a button that will give up a form
